8c7ae11cbc28cffb9c46216bb15857f6254064e1
Andrew <andrew.vinocur@gmail.com>
2017-12-03 12:15:44 -0800

    initial logic for rolling3 dice and jquery to change html on each

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,20 @@
 console.log('enchiladas');
+
+
+function roll () {
+
+  for (i = 0; i < 3; i++){
+    let n = Math.random();
+
+    if (n <= .333 ){
+      // console.log(0);
+      $('#row'):nth-child(i).html(0);
+    } else if (n <= .666) {
+      // console.log(-1);
+      $('#row'):nth-child(i).html(-1);
+    } else {
+      // console.log(1);
+      $('#row'):nth-child(i).html(1);
+    }
+  }
+}
